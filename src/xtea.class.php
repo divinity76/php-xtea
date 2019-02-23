9a77619896509fd41bd4cebe7ee2159c4afc0537
divinity76 <divinity76@gmail.com>
2019-02-23 17:32:03 +0100

    make _add protected

--- src/xtea.class.php
+++ src/xtea.class.php
@@ -302,7 +302,7 @@ declare (strict_types = 1);
      *  @since          2004/Sep/06
      *  @author         Jeroen Derks <jeroen@derks.it>
      */
-    function _add($i1, $i2)
+    protected function _add($i1, $i2)
     {
         $result = 0.0;
         foreach ([$i1, $i2] as $value) {
